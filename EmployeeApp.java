93f3e49ba5ee495e893ae46940cead4fa6a97b38
alfaraon <lr.alfaraon@mmdc.mcl.edu.ph>
2025-05-15 23:56:33 +0800

    Add files via upload

--- EmployeeApp.java
+++ EmployeeApp.java
@@ -2,11 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package employee.app;
+package employeeapp;
 
 /**
  *
- * @author jonhfrancis
+ * @author alfaraon
  */
 public class EmployeeApp {
 
@@ -17,4 +17,47 @@ public class EmployeeApp {
         // TODO code application logic here
     }
     
+}// Employee.java
+public class Employee {
+    private int employeeId;
+    private String name;
+    private String position;
+    private double salary;
+
+    // Getters and setters
 }
+
+// Attendance.java
+public class Attendance {
+    private int attendanceId;
+    private Employee employee;
+    private Date date;
+    private Time timeIn;
+    private Time timeOut;
+
+    // Getters and setters
+}
+
+// Payroll.java
+public class Payroll {
+    private int payrollId;
+    private Employee employee;
+    private String payPeriod;
+    private double grossPay;
+    private double deductions;
+    private double netPay;
+
+    // Getters and setters
+}
+
+// Leave.java
+public class Leave {
+    private int leaveId;
+    private Employee employee;
+    private String leaveType;
+    private Date startDate;
+    private Date endDate;
+
+    // Getters and setters
+}
+
